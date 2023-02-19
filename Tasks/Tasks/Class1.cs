cc185c51ad2f8e7fdd59e812a6a2eaf4b2b4442c
_Kirill_ <kirillagafonov28@gmail.com>
2023-02-19 19:53:52 +0300

    Tsks1

--- Tasks/Tasks/Class1.cs
+++ Tasks/Tasks/Class1.cs
@@ -17,7 +17,19 @@ namespace Tasks
             Console.WriteLine($"A/B = {A / B}");
             Console.WriteLine($"Остаток от деления = {A % B}");
 
+        }
+        public void SecondTask()
+        {
+            Console.WriteLine("Введите число A:");
+            int A = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Введите число B:");
+            int B = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("Вычисляю...");
+            Console.WriteLine();
+            Console.WriteLine($"{(5 * A + B * B) / (B - A)}");
         }
     }
+
 }
