1645a2c3ea1745d1f47000c9af90798318cde75e
_Kirill_ <kirillagafonov28@gmail.com>
2023-02-19 19:58:00 +0300

    Tsks2

--- Tasks/Tasks/Class1.cs
+++ Tasks/Tasks/Class1.cs
@@ -30,6 +30,21 @@ namespace Tasks
             Console.WriteLine();
             Console.WriteLine($"{(5 * A + B * B) / (B - A)}");
         }
+
+        public void ThirdTask()
+        {
+            Console.Write("Введите сумму вклада Y: ");
+            decimal Y = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Под какой процент годовых? X: ");
+            decimal X = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Вычисляю...");
+            Console.WriteLine();
+            Console.WriteLine($"Количество денег,которые принесет ваш вклад за 1 месяц = {X * ((Y / 100) / 12)} ");
+        }
     }
+
 
 }
