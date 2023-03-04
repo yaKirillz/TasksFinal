06126bb7587ab216466cd5d0a5b9d1e1405d9bab
_Kirill_ <kirillagafonov28@gmail.com>
2023-03-04 16:00:04 +0300

    Tests

---
+++ Tasks/MyCalc/Calc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace MyCalc
+{
+    public class Calc
+    {
+        public static int calc(int a, int b, int c)
+        {
+            return ((a + b) / c);
+        }
+
+        public static int calc1(int a, int b)
+        {
+            return a % b;
+        }
+    }
+}
